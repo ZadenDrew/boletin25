ff6b53a6feb29f5d528b94bd617c515ba07af4a7
acabezaslopez <acabezaslopez@victoria11.danielcastelao.org>
2018-05-11 13:31:08 +0200

    Bonoloto

---
+++ src/boletin25/Boletin25.java
@@ -0,0 +1,18 @@
+
+package boletin25;
+
+/**
+ *
+ * @author acabezaslopez
+ */
+public class Boletin25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Bonoloto b=new Bonoloto();
+       b.setVisible(true);
+    }
+    
+}
